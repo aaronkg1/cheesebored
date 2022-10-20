296bed7ecdde40f6d04ae625e1b10ec0d64f5ac6
aaronkg1 <aaronkg1@gmail.com>
2022-10-20 10:35:35 +0100

    feat: Replaced ul with bootstrap components

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 const SiteNavbar = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="cheeses">Cheeses</Link>
-      </li>
-    </ul>
+    <Navbar expand="sm" bg="dark">
+      <Container>
+        <Navbar.Brand href="/">🧀</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link className="btn btn-warning p-2" href="/cheeses">
+            Discover Cheeses
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
